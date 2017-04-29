306f02a94ef20d5bba55f0d269587c02d6bc3caa
Silviu Eigel <sheigel@sheigel.local>
2017-04-29 13:02:59 +0300

    implement linked list add and reverse

---
+++ reverse_linked_list.go
@@ -0,0 +1,51 @@
+package algo
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func tailPointer(current **Node) **Node {
+	if (*current) == nil {
+		return current
+	}
+	return tailPointer(&((*current).next))
+}
+
+func newNodeOf(val int) *Node {
+	return &Node{value: val}
+}
+
+func (l *LinkedList) Add(value int) {
+	tail := tailPointer(&l.head)
+
+	*tail = newNodeOf(value)
+}
+
+func reverseRec(previous, current *Node) *Node {
+	if current == nil {
+		return previous
+	}
+	next := current.next
+	current.next = previous
+	return reverseRec(current, next)
+}
+
+func (l *LinkedList) Reverse() {
+	l.head = reverseRec(nil, l.head)
+	//var previous *Node
+	//current := l.head
+	//
+	//for current != nil {
+	//	l.head = l.head.next
+	//	current.next = previous
+	//	previous = current
+	//	current = l.head
+	//}
+	//
+	//l.head = previous
+}
